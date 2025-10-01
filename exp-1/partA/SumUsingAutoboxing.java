b3f8e7594edbb7629c433618d107577031402d0a
Varun Aggarwal <161594259+Varunggarwal@users.noreply.github.com>
2025-10-01 11:51:41 +0530

    Implement sum calculation using autoboxing

--- exp-1/partA/SumUsingAutoboxing.java
+++ exp-1/partA/SumUsingAutoboxing.java
@@ -1,1 +1,25 @@
-dfvdfv
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumUsingAutoboxing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        String input = sc.nextLine();
+        String[] tokens = input.split(" ");
+
+        for (String token : tokens) {
+            int num = Integer.parseInt(token);
+            numbers.add(num);
+        }
+
+        int sum = 0;
+        for (Integer num : numbers) {
+            sum += num;
+        }
+
+        System.out.println("Sum of integers: " + sum);
+        sc.close();
+    }
+}
